a46568e0fa3e0649dedc54a86164c7687f76a638
Makoto Hasegawa <ec2-user@ip-172-31-34-84.us-east-2.compute.internal>
2018-05-30 01:28:01 +0000

    CRUD messages

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+{!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'タスク:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('投稿') !!}
+
+    {!! Form::close() !!}
+
+@endsection
